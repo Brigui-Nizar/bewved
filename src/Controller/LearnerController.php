87218262b957e424a632f408f9389025cc106e09
nizar <brigui.nizar@gmail.com>
2023-07-18 15:09:02 +0000

    learnerController + twig + dto

--- src/Controller/LearnerController.php
+++ src/Controller/LearnerController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Learner;
+use App\Form\LearnerType;
 use App\Repository\LearnerRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,5 +21,53 @@ class LearnerController extends AbstractController
         return $this->render('learner/list.html.twig', [
             'learners' => $learners,
         ]);
+    }
+
+    /**
+     * create Learner
+     */
+    #[Route('/learner/create', name: 'app_learner_create')]
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $learner = new Learner;
+
+        $form = $this->createForm(LearnerType::class, $learner);
+
+        $form->handleRequest($request);
+
+        if (!$form->isSubmitted() || !$form->isValid()) {
+            return $this->render('learner/createLearner.html.twig', [
+                'form' => $form->createView(),
+            ]);
+        }
+        $learner = $form->getData();
+
+        $entityManager->persist($learner);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_learner_index');
+    }
+    /**
+     * Learner getOne
+     */
+    #[Route('/learner/{id}', name: 'app_learner_getOne')]
+    public function getOne(Learner $learner): Response
+    {
+
+
+        return $this->render('learner/detail.html.twig', [
+            'learner' => $learner,
+        ]);
+    }
+    /**
+     * Learner delete
+     */
+    #[Route('/learner/delete/{id}', name: 'app_learner_delete')]
+    public function delete(Learner $learner, EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->remove($learner);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_learner_index');
     }
 }
